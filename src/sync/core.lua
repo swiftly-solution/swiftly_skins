bc25a9d6ddc75bde5cf93f3e1f8bd47fd75d4dbb
criskkky <36176343+criskkky@users.noreply.github.com>
2024-03-24 17:48:26 -0300

    Auto data cleanup

--- src/sync/core.lua
+++ src/sync/core.lua
@@ -22,3 +22,46 @@ function SyncPlayersWithDB()
         ::sync_continue::
     end
 end
+
+function CheckPlayerConnection(pl)
+    if not db then return end
+    local player = pl
+    local steamid = tostring(player:GetSteamID())
+    local result = db:Query("select lastConnection from sw_skins_users where steamid = '".. steamid .."'")
+    if #result == 0 then return end
+
+    local lastConnection = result[1].lastConnection
+    local currentTime = os.time()
+    local diff = currentTime - lastConnection
+    time = (config:Fetch("skins.daysToErase") * 86400)
+
+    if time ~= nil and diff > time then
+        db:Query("delete from sw_skins_users where steamid = '".. steamid .."'")
+    end
+end
+
+function CheckPlayerConnection()
+    for i=0,playermanager:GetPlayerCap()-1,1 do 
+        local player = GetPlayer(i)
+        if not player then goto check_continue end
+        CheckPlayerConnection(player)
+        ::check_continue::
+    end
+end
+
+function RegisterLastConnection(pl)
+    if not db then return end
+    local player = pl
+    local steamid = tostring(player:GetSteamID())
+    local currentTime = os.time()
+    db:Query("update sw_skins_users set lastConnection = '".. currentTime .."' where steamid = '".. steamid .."'")
+end
+
+function RegisterLastConnection()
+    for i=0,playermanager:GetPlayerCap()-1,1 do 
+        local player = GetPlayer(i)
+        if not player then goto register_continue end
+        RegisterLastConnection(player)
+        ::register_continue::
+    end
+end
